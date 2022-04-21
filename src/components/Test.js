cb60f2b32086e8e5f966c9a5175cf7f764f51610
autodidactor <54153428+autodidactor@users.noreply.github.com>
2022-04-21 21:00:04 +0200

    remove console logs

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom"
 
 export default function Test({data}) {
-    console.log(data)
   return (
     <div>
         {data.map((board) => (
-            <div key={board.boardId}>{console.log(board)}
+            <div key={board.boardId}>
                 <p>{board.boardName}</p>
                 <p>{board.boardId}</p>
                 <Link to={`/board/${board.boardId}`}>Open this board</Link>
-                {board.columns.map((column)=>console.log(column))}
-                {console.log(board.columns)}
             </div>
         ))}
         a
