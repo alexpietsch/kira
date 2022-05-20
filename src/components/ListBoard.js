03065a29b7e26565e7b909230a0203886d1d8789
alexpietsch <54153428+alexpietsch@users.noreply.github.com>
2022-05-20 18:57:06 +0200

    add column delete function

--- src/components/ListBoard.js
+++ src/components/ListBoard.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { useCollection } from "../hooks/useCollection"
 import { useFirestore } from "../hooks/useFirestore"
-import { timestamp } from "../firebase/config"
+import { useHistory } from "react-router-dom";
 import TaskAdd from "./TaskAdd";
 import "./ListBoard.css"
 import { v4 as uuidv4 } from "uuid"
@@ -45,6 +45,7 @@ export default function ListBoard() {
 
     const { user } = useAuthContext()
     const { id } = useParams()
+    const history = useHistory();
     const { documents, error } = useCollection(
         "tasks_new_structure", 
         ["boardID", "==", id],
@@ -61,10 +62,14 @@ export default function ListBoard() {
 
     
     const [boardData, setBoardData] = useState(null)
+
+    const [isEditColumnOpen, setIsEditColumnOpen] = useState(false)
     const [isTaskAddModalOpen, setIsTaskAddModalOpen] = useState(false)
     const [isColumnAddOpen, setIsColumnAddOpen] = useState(false)
     const [isCardModalOpen, setIsCardModalOpen] = useState(false)
     const [modalActiveColumn, setModalActiveColumn] = useState(null)
+    const [showError, setShowError] = useState(false)
+    const [showConfirmModal, setShowConfirmModal] = useState(false)
 
     const [newColumnName, setNewColumnName] = useState("")
 
@@ -124,8 +129,23 @@ export default function ListBoard() {
         setBoardData(newState)
         updateDocument(boardData.boardID, {columns: newState.columns})
     }
+    function handleDeleteColumn(){
+        let newState = boardData
+        const isTargetColumn = (column) => column.columnID === modalActiveColumn
+        const indexOfColumn = newState.columns.findIndex(isTargetColumn)
+        // remove the column from the columns array
+        newState.columns.splice(indexOfColumn, 1)
+        setBoardData(newState)
+        setShowConfirmModal(false)
+        setIsEditColumnOpen(false)
+        updateDocument(boardData.boardID, {columns: newState.columns})
+    }
     function handleAddColumn(e){
         e.preventDefault()
+        if (!newColumnName) {
+            setShowError(true)
+            return
+        }
         const column = { 
             columnID: uuidv4(),
             columnName: newColumnName,
@@ -133,13 +153,15 @@ export default function ListBoard() {
         }
         let columns = boardData.columns
         columns.push(column)
+        setShowError(false)
         updateDocument(boardData.boardID, {columns: columns})
         setIsColumnAddOpen(false)
         setNewColumnName("")
     }
   return (
     <>
-    {boardData && <p>Projektname: {boardData.boardName}</p>}
+
+    {/* Modals */}
     {isTaskAddModalOpen && 
         <Modal customWidth={"80%"} isCenter={true}>
             <TaskAdd boardData={boardData} sourceColumnID={modalActiveColumn} setBoardData={setBoardData} setIsTaskAddModalOpen={setIsTaskAddModalOpen} />
@@ -149,12 +171,57 @@ export default function ListBoard() {
             <h1>Edit and view card here</h1>
             <button onClick={() => setIsCardModalOpen(false)} className="button-dark">Close</button>
         </Modal>}
+    {isEditColumnOpen &&
+        <Modal customWidth={"80%"} isCenter={true}>
+            <h1>Edit and view column here</h1>
+            <ul style={{listStyle: "none"}}>
+                <li>
+                <button className="button-danger" onClick={() => {
+                        setShowConfirmModal(true)
+                }}>Delete Column</button>
+                </li>
+            </ul>
+            <button onClick={() => setIsEditColumnOpen(false)} className="button-dark">Close</button>
+        </Modal>}
+
+    {showConfirmModal && 
+        <Modal customWidth={"30%"}>
+            <h1>Delete Column?</h1>
+            <button className="button-danger" onClick={() => handleDeleteColumn()}>Yes</button>
+            <button className="button-dark" onClick={() => {
+                setShowConfirmModal(false)
+                setModalActiveColumn(null)
+            }}>No</button>
+        </Modal>}
+    {/* End Modals */}
+
+
+    <span style={{
+        position: "absolute",
+        top: "70px",
+        left: "5px",
+        fontSize: "3em",
+        cursor: "pointer"
+    }}
+    onClick={() => {
+        history.push("/")
+    }}>&#8592;</span>
+    {boardData && <p>Projektname: {boardData.boardName}</p>}
     <div className="list-container" style={columnWidthDiff}>
         {!error && <>
             <DragDropContext onDragEnd={handleOnDragEnd}>
                 {boardData && boardData.columns.map((column, index) => (
                     <div key={column.columnID} className="taskColumn" style={columnWidth}>
-                        <h2>{column.columnName}</h2>
+                        <h2>
+                            {column.columnName}
+                            <button onClick={() => {
+                                    setIsEditColumnOpen(true)
+                                    setModalActiveColumn(column.columnID)
+                                    }} 
+                                    className="button-dark">
+                                &#128393;
+                            </button>
+                        </h2>
                         <Droppable droppableId={column.columnID}>
                         {(provided) => (
                         <>
@@ -191,12 +258,17 @@ export default function ListBoard() {
                 ))}
             </DragDropContext>
             <div className="taskColumn" style={{paddingTop: "4.5em"}}>
-                <button style={{marginBottom: "1.4em"}} onClick={() => setIsColumnAddOpen(true)} className="button-dark">+ New Column</button>
+                {!isColumnAddOpen && <button style={{marginBottom: "1.4em"}} onClick={() => setIsColumnAddOpen(true)} className="button-dark">+ New Column</button>}
+                {isColumnAddOpen && <button style={{marginBottom: "1.4em"}} onClick={() => {
+                    setIsColumnAddOpen(false)
+                    setNewColumnName("")
+                }} className="button-dark">Cancel</button>}
                 {isColumnAddOpen &&
                     <div className="column">
                         <form onSubmit={handleAddColumn}>
                             <label>
                                 <span>Column Name:</span>
+                                {showError && <><br/><span style={{color: "red"}}>Column Name can't be empty</span></>}
                                 <input type="text" placeholder="Column Name" value={newColumnName} onChange={(e) => setNewColumnName(e.target.value)} />
                             </label>
                             <button type="submit" className="button-dark">add Column</button>
