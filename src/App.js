f35aa0c45ffae6b27139caf8093022ff523f93a0
alexpietsch <54153428+alexpietsch@users.noreply.github.com>
2022-09-22 22:20:12 +0200

    fix routing when user isn't logged in

--- src/App.js
+++ src/App.js
@@ -64,6 +64,22 @@ function App() {
               } />
             </>
           )}
+
+          {!user && (
+              <>
+                <Route path="/login" element={
+                  <Login />
+                } />
+
+                <Route path="/signup" element={
+                  <Signup />
+                } />
+
+                <Route path="*" element={
+                  <Navigate to="/login" />
+                } />
+              </>
+          )}
         </Routes>
         <Footer />
       </BrowserRouter>
